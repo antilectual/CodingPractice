839d02a52c4a62fe0d8e9ba0823d4b5077021889
Matt <matthewwjohnson@nevada.unr.edu>
2020-01-14 08:18:02 -0800

    Comment clarification about confusion on the problem statement.

--- LeetCode/1281. Subtract the Product and Sum of Digits of an Integer.cpp
+++ LeetCode/1281. Subtract the Product and Sum of Digits of an Integer.cpp
@@ -1,12 +1,13 @@
 //1281. Subtract the Product and Sum of Digits of an Integer
+// Note:Question states "Given an integer number n, return the difference between the product of its digits and the sum of its digits.".
+// However, submission requires the subtraction and not the absolute difference, so absolute value is not used in this solution.
 class Solution {
 public:
     int subtractProductAndSum(int n) {
         int tempNum = n, sum = 0, product = 1, current=0;
         while(tempNum > 0)
         {
-            current = tempNum%10;
-            
+            current = tempNum%10;     
             sum += current;
             product *= current;
             tempNum = tempNum/10;
