6df8684bb1c5b0f977a658931bdb3400106bf706
Matt <matthewwjohnson@nevada.unr.edu>
2020-01-18 07:54:30 -0800

    938. Range Sum of BST (easy) - Brute force [SLOW]

---
+++ LeetCode/938. Range Sum of BST (easy).cpp
@@ -0,0 +1,30 @@
+// 938. Range Sum of BST (easy)
+// Brute Force Method by checking every value in the binary tree.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) 
+    {
+        int sumResult = 0;
+        if(root == NULL)
+        {
+            return 0;
+        }
+        else if(root->val >= L && root->val <= R)
+        {
+            sumResult += root->val;
+        }
+        int x = root->val;
+        sumResult += rangeSumBST(root->left, L, R);
+        sumResult += rangeSumBST(root->right, L, R);
+        return sumResult;
+    }
+};
