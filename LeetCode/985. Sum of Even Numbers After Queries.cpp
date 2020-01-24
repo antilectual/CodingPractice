887154e96ba5e29b0dd89dcd3051466f7bba016a
Matt <matthewwjohnson@nevada.unr.edu>
2020-01-23 18:10:09 -0800

    Speed optimization by initializing the size of the vector rather than increasing size by 1 every iteration.  Halves the run time.

--- LeetCode/985. Sum of Even Numbers After Queries.cpp
+++ LeetCode/985. Sum of Even Numbers After Queries.cpp
@@ -2,22 +2,40 @@
 
 // Initial "solution".  Recalculate full sum of even values in A for every query.
 // O(A * queries).  Exceeds time limit.
+
 class Solution {
 public:
     vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& queries) {
-        vector<int> answer; 
+        vector<int> answer(queries.size()); 
+        int sumA = 0;
+        // find the initial SUM of evens
+        for(int i = 0; i < A.size(); i++)
+        {
+            if(A[i] % 2 == 0)
+            {
+                sumA += A[i];
+            }
+        }
+        
+        //iterate the queries modifying values in A modifying A's sum of evens result (sumA)
         for(int i = 0; i < queries.size(); i++)
         {
-            int tempSum = 0;
-            A[queries[i][1]] += queries[i][0];
-            for(int j = 0; j < A.size(); j++)
+            // subtract even values from the current index
+            if((A[queries[i][1]]) % 2 == 0)
             {
-                if(A[j] % 2 == 0)
-                {
-                    tempSum += A[j];
-                }
+                sumA -= A[queries[i][1]];
             }
-            answer.push_back(tempSum);
+            
+            // modify the appropriate index to the appropriate value (adding)
+            A[queries[i][1]] += queries[i][0];
+            
+            // if the current index is modified to be even, add it to the sum
+            if(A[queries[i][1]] % 2 == 0)
+            {
+                sumA += A[queries[i][1]];
+            }
+            
+            answer[i] = sumA;
         }
         return answer;
     }
