f1020ad60efed1eeaba7e42929ee70cfed9967bf
Matt <matthewwjohnson@nevada.unr.edu>
2020-01-26 08:33:23 -0800

    significant speed optimization

--- LeetCode/1299. Replace Elements with Greatest Element on Right Side (easy).cpp
+++ LeetCode/1299. Replace Elements with Greatest Element on Right Side (easy).cpp
@@ -2,14 +2,15 @@
 
 // Algorithm: iterate the array from right to left finding the largest value and replacing the current value in the same index in a copy of the array
 // Note: does not modify the array arr.  Would require copying elements from the copy back to arr.
+// Update: Pre-sized the array and used indexes rather than emplace to speed up runtime significantly. (tests @ 340ms -> 48ms)
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        vector<int> replacedElements;
+        vector<int> replacedElements(arr.size());
         int currentMax = -1;
         for(int i = arr.size() -1; i >= 0; i--)
         {
-            replacedElements.emplace(replacedElements.begin(), currentMax);
+            replacedElements[i] = currentMax;
             if(arr[i] > currentMax)
             {
                 currentMax = arr[i];
