5b1e91b92576d1fd60f25f8de1a1382eb8e1142b
Matt <matthewwjohnson@nevada.unr.edu>
2020-01-28 09:57:43 -0800

    Revisions for a much faster solution by using cases and group by instead of joins

--- LeetCode/1179. Reformat Department Table (easy).sql
+++ LeetCode/1179. Reformat Department Table (easy).sql
@@ -1,43 +1,21 @@
 # LeetCode problem 1179. Reformat Department Table (easy)
+# Revisions based on discussions
+# Note: SUM used as an aggregate so NULLs for revenue on a given month are not chosen.
+#
 
-# Write your MySQL query statement below
 SELECT
-    Department.id, 
-    DeptJan.revenue AS Jan_Revenue,
-    DeptFeb.revenue AS Feb_Revenue,
-    DeptMar.revenue AS Mar_Revenue,
-    DeptApr.revenue AS Apr_Revenue,
-    DeptMay.revenue AS May_Revenue,
-    DeptJun.revenue AS Jun_Revenue,
-    DeptJul.revenue AS Jul_Revenue,
-    DeptAug.revenue AS Aug_Revenue,
-    DeptSep.revenue AS Sep_Revenue,
-    DeptOct.revenue AS Oct_Revenue,
-    DeptNov.revenue AS Nov_Revenue,
-    DeptDec.revenue AS Dec_Revenue
+	id, 
+	SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+	SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+	SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+	SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+	SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+	SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+	SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+	SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+	SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+	SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+	SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+	SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
 FROM Department
-LEFT JOIN Department AS DeptJan 
-ON Department.id = DeptJan.id AND DeptJan.month = "Jan"
-LEFT JOIN Department AS DeptFeb 
-ON Department.id = DeptFeb.id AND DeptFeb.month = "Feb"
-LEFT JOIN Department AS DeptMar 
-ON Department.id = DeptMar.id AND DeptMar.month = "Mar"
-LEFT JOIN Department AS DeptApr 
-ON Department.id = DeptApr.id AND DeptApr.month = "Apr"
-LEFT JOIN Department AS DeptMay 
-ON Department.id = DeptMay.id AND DeptMay.month = "May"
-LEFT JOIN Department AS DeptJun 
-ON Department.id = DeptJun.id AND DeptJun.month = "Jun"
-LEFT JOIN Department AS DeptJul 
-ON Department.id = DeptJul.id AND DeptJul.month = "Jul"
-LEFT JOIN Department AS DeptAug 
-ON Department.id = DeptAug.id AND DeptAug.month = "Aug"
-LEFT JOIN Department AS DeptSep 
-ON Department.id = DeptSep.id AND DeptSep.month = "Sep"
-LEFT JOIN Department AS DeptOct 
-ON Department.id = DeptOct.id AND DeptOct.month = "Oct"
-LEFT JOIN Department AS DeptNov 
-ON Department.id = DeptNov.id AND DeptNov.month = "Nov"
-LEFT JOIN Department AS DeptDec 
-ON Department.id = DeptDec.id AND DeptDec.month = "Dec"
-GROUP BY Department.id;
+GROUP BY id
