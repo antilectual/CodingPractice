8b17bc967cf5272db38f30f9cf8697a41b2ca41a
Matt <matthewwjohnson@nevada.unr.edu>
2020-02-01 20:16:02 -0800

    Converted to a special case binary-search. O(log(n)) vs O(n)

--- LeetCode/852. Peak Index in a Mountain Array (easy).cpp
+++ LeetCode/852. Peak Index in a Mountain Array (easy).cpp
@@ -1,12 +1,42 @@
 // LeetCode problem 852. Peak Index in a Mountain Array (easy)
-// Algorithm: Simple solution: Problem states A is a "mountain", thus we only need to find when values stop increasing. Iterates from start of array.
+// Algorithm: special case binary-search for peak value;
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        int i = 0;
-        while(A[i] <= A[i+1])
+        int i = (A.size() - 1) / 2;
+		int max = A.size() - 1;
+		int min = 0;
+        while(true)
         {
-            i++;
+			if(i > 0 && i < A.size() - 1)
+			{
+				if(A[i] > A[i + 1] && A[i] > A[i-1]) // case: peak is found
+				{
+					break;
+				}
+				else if(A[i] > A[i-1]) // case: we need to traverse right
+				{
+					min = i;
+					i = (i + max) / 2;
+                    if(i == min) // case: getting i + 0.5
+                    {
+                        i++;
+                    }
+				}
+				else // case: we need to traverse left
+				{
+					max = i;
+					i = (i + min) / 2;
+                    if(i == max) // case: getting i - 0.5
+                    {
+                        i--;
+                    }
+				}
+			}
+			else // case: top of the mountain is start/end
+			{
+				break;
+			}
         }
         return i;
     }
