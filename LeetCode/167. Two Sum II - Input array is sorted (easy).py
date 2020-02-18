abbaaa7f2ce4d0fc437978cedfaf7593d6da098a
Matt <matthewwjohnson@nevada.unr.edu>
2020-02-17 23:29:30 -0800

    Modified algorithm. 6456ms -> 68 ms

--- LeetCode/167. Two Sum II - Input array is sorted (easy).py
+++ LeetCode/167. Two Sum II - Input array is sorted (easy).py
@@ -1,14 +1,13 @@
 # LeetCode problem 167. Two Sum II - Input array is sorted (easy)
-# Algorithm: Choose a number in the numbers array. Iterate the numbers array between the value and the target - value. Return when the solution is found 
+# Algorithm: Iterate from the front and back at the same time to approach the correct pair of values
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        for i,n in enumerate(numbers):
-            j = i + 1
-            end = len(numbers) - 1
-            tempTarget = target - n
-            while(j < end and numbers[j] < tempTarget):
-                j += 1
-            if(numbers[i] + numbers[j] == target):
-                return [i+1,j+1]
-        return
+        j = len(numbers) - 1
+        i = 0
+        while(numbers[i] + numbers[j] != target):
+            if(numbers[i] + numbers[j] > target):
+                j -= 1
+            if(numbers[i] + numbers[j] <target):
+                i += 1
+        return [i+1, j+1]
