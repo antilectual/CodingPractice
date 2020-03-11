af544c5aba7dec04915b8b7ad059df921fcc0b70
Matt <matthewwjohnson@nevada.unr.edu>
2020-03-11 15:50:06 -0700

    LeetCode problem 476. Number Complement.py

---
+++ LeetCode/476. Number Complement.py
@@ -0,0 +1,16 @@
+# LeetCode problem 1009. Complement of Base 10 Integer
+# LeetCode problem 476. Number Complement
+
+# Algorithm: XOR each bit from least significant to most significant bit and add to the result.
+
+class Solution:
+    def bitwiseComplement(self, N: int) -> int:
+        count = 0
+        result = 0
+        if N == 0:
+            return 1
+        while N > 0:
+            result += (((N % 2) ^ 1) << count)
+            count += 1
+            N //= 2
+        return result
